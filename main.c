c3e11fb1015a266a4ee930bbef0437b5636c1147
AntoniaAcevedo <antonia.acevedo.p@mail.cl>
2022-06-15 09:31:38 -0400

    avance main

--- main.c
+++ main.c
@@ -3,8 +3,48 @@
 
 #include "funciones.h"
 
-int main(void)
+#define MAX 100
+
+//Estudiante * usuario = create_usuario;
+int main (void)
 {
-    // Menu
-    return 0;
+    while (1){
+
+        long selec;
+        long selec2;
+        char arch[MAX] = "";
+
+        system("cls");
+        printf("Iniciar sesion\n\n");
+
+        printf(
+            "1. Rut\n"
+            "2. contraseña\n"
+        );
+
+        scanf("%ld", &selec);
+        getchar();
+
+        switch (selec)
+        {
+            case 1:
+                system("cls");
+                puts("Ingrese Rut");
+                scanf("%s", arch);
+                getchar();
+                //buscar_Rut(arch,usuario)
+                getchar();
+                break;
+            case 2:
+                system("cls");
+                puts("Ingrese Contraseña");
+                scanf("%s", arch);
+                getchar();
+                //buscar_Contraseña(arch,usuario)
+                getchar();
+            break;
+
+            
+        }
+    }
 }
